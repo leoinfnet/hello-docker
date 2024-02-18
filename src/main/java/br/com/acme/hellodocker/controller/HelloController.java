f7ea20decfda1fb35b07c9424faf4fb36a079d4c
leonardogloria <contato@leonardogloria.com.br>
2024-02-18 20:05:10 -0300

    Controller

--- src/main/java/br/com/acme/hellodocker/controller/HelloController.java
+++ src/main/java/br/com/acme/hellodocker/controller/HelloController.java
@@ -1,14 +1,22 @@
 package br.com.acme.hellodocker.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Map;
+
 @RestController
 @RequestMapping("/hello")
 public class HelloController {
+    Logger logger = LoggerFactory.getLogger(HelloController.class);
     @GetMapping
-    public void hello(){
-        System.out.println("Hello from Docker!!!");
+    public ResponseEntity<?> hello(){
+        logger.info("Hello From Docker");
+        return ResponseEntity.ok(Map.of("message", "Hello From Docker"));
+
     }
 }
